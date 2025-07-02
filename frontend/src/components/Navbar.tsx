f2e7bc6569d345b345a4e07856f6c080e90a4aa4
rrndxx <rendyllcabardo11@gmail.com>
2025-07-02 21:31:05 +0800

    .

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { LogOut, Moon, Settings, User } from "lucide-react"
+import { LogOut, Settings, User } from "lucide-react"
 import { Link } from "react-router-dom"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import {
