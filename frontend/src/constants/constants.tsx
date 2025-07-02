5d58c7eff6dc649b0edc2a5e9ba8787476e98ef1
rrndxx <rendyllcabardo11@gmail.com>
2025-07-02 20:11:18 +0800

    balik uno

--- frontend/src/constants/constants.tsx
+++
@@ -1,7 +0,0 @@
-import { ArrowUpDown, Home, Smartphone } from "lucide-react";
-
-export const navs = [
-  { id: 1, title: "Home", icon: <Home size={24} />, href: "/dashboard" },
-  { id: 2, title: "Connected Devices", icon: <Smartphone size={24} />, href: "/devices" },
-  { id: 3, title: "Bandwidth Usage", icon: <ArrowUpDown size={24} />, href: "/bandwidth" },
-];
