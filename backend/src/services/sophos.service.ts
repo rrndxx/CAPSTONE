dec4be28fa697587e5e0ba00e3468e6af863abdc
rrndxx <rendyllcabardo11@gmail.com>
2025-07-24 22:55:05 +0800

    deleted backend

--- backend/src/services/sophos.service.ts
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-import { parseXMLFile } from '../utils/xmlParser';
-
-export const getAllDevices = async () => {
-  const filePath = path.join(__dirname, '..', 'sample_api', 'devices.xml');
-  const data = await parseXMLFile(filePath);
-  return data.Devices.Device || [];
-};
